6a9de10c6e0b2f2fad0069bc0101328d6b2dede5
Andreas Calleja <andreascalleja@gmail.com>
2023-11-07 15:43:58 +0100

    Added all team endpoints

--- app/routers/team.py
+++ app/routers/team.py
@@ -1,9 +1,6 @@
-from fastapi import status, APIRouter, HTTPException, Response, Depends, Request
-from sqlalchemy import func
+from fastapi import status, APIRouter, HTTPException, Response, Depends
 from sqlalchemy.orm import Session
 from typing import List
-from pydantic import create_model
-import random
 
 from app.database import get_db
 import app.schemas as schemas
@@ -35,7 +32,18 @@ def create_team(
 def get_teams(
     db: Session = Depends(get_db),
     limit: int = 10,
+    current: bool | None = None,
 ):
+    if current != None:
+        results = (
+            db.query(models.Team)
+            .order_by(models.Team.id)
+            .filter(models.Team.current_team == current)
+            .limit(limit)
+            .all()
+        )
+        return results
+
     results = db.query(models.Team).order_by(models.Team.id).limit(limit).all()
     return results
 
@@ -53,62 +61,42 @@ def get_team(id: int, db: Session = Depends(get_db)):
     return team
 
 
-# @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT, include_in_schema=False)
-# def delete_team(
-#     request: Request,
-#     id: int,
-#     current_user: int = Depends(oauth2.get_current_user),
-#     db: Session = Depends(get_db),
-# ):
-#     if current_user.id != 1:
-#         raise HTTPException(
-#             status_code=status.HTTP_401_UNAUTHORIZED,
-#             detail=f"Unauthorized action",
-#         )
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT, include_in_schema=False)
+def delete_team(
+    id: int,
+    db: Session = Depends(get_db),
+):
+    team_query = db.query(models.Team).filter(models.Team.id == id)
+    team = team_query.first()
 
-#     team_query = db.query(models.Team).filter(models.Team.id == id)
-#     team = team_query.first()
+    if team == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Team with id {id} was not found",
+        )
 
-#     if team == None:
-#         raise HTTPException(
-#             status_code=status.HTTP_404_NOT_FOUND,
-#             detail=f"Team with id {id} was not found",
-#         )
+    team_query.delete(synchronize_session=False)
+    db.commit()
 
-#     team_query.delete(synchronize_session=False)
-#     db.commit()
-
-#     post_request(db, "teams", request)
-
-#     return Response(status_code=status.HTTP_204_NO_CONTENT)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-# @router.put("/{id}", response_model=schemas.TeamResponse, include_in_schema=False)
-# def update_team(
-#     request: Request,
-#     id: int,
-#     updated_team: schemas.TeamCreate,
-#     current_user: int = Depends(oauth2.get_current_user),
-#     db: Session = Depends(get_db),
-# ):
-#     if current_user.id != 1:
-#         raise HTTPException(
-#             status_code=status.HTTP_401_UNAUTHORIZED,
-#             detail=f"Unauthorized action",
-#         )
+@router.put("/{id}", response_model=schemas.TeamResponse, include_in_schema=False)
+def update_team(
+    id: int,
+    updated_team: schemas.TeamCreate,
+    db: Session = Depends(get_db),
+):
+    team_query = db.query(models.Team).filter(models.Team.id == id)
+    team = team_query.first()
 
-#     team_query = db.query(models.Team).filter(models.Team.id == id)
-#     team = team_query.first()
+    if team == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Team with id {id} was not found",
+        )
 
-#     if team == None:
-#         raise HTTPException(
-#             status_code=status.HTTP_404_NOT_FOUND,
-#             detail=f"Team with id {id} was not found",
-#         )
+    team_query.update(updated_team.dict(), synchronize_session=False)
+    db.commit()
 
-#     team_query.update(updated_team.dict(), synchronize_session=False)
-#     db.commit()
-
-#     post_request(db, "teams", request)
-
-#     return team_query.first()
+    return team_query.first()
