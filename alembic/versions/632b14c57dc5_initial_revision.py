c15395fd44beb5efe547e8e710bad15f83e24ef6
Andreas Calleja <andreascalleja@gmail.com>
2023-11-14 17:30:53 +0100

    Updated alembic revisions

--- alembic/versions/632b14c57dc5_initial_revision.py
+++ alembic/versions/632b14c57dc5_initial_revision.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '632b14c57dc5'
+revision: str = "632b14c57dc5"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
