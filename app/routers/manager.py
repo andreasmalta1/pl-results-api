3a0a2dbc2a43afccb0050f548daa14b04c498a9b
Andreas Calleja <andreascalleja@gmail.com>
2023-11-24 21:37:07 +0100

    Updated manager endpoints

--- app/routers/manager.py
+++ app/routers/manager.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 
 from app.auth import get_api_key, authorization_error
 from app.database import get_db
-from app.models import Team, Nation, Manager
+from app.models import Nation, Manager
 import app.schemas as schemas
 
 
@@ -28,38 +28,15 @@ def create_manager(
         authorization_error()
 
     new_manager = Manager(**manager.model_dump())
-    team_id = new_manager.team
-    nation_id = new_manager.nationality
+    nation_id = new_manager.nation_id
 
-    team = db.query(Team).filter(Team.id == team_id).first()
     nation = db.query(Nation).filter(Nation.id == nation_id).first()
-
-    if not team:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Team with id {team_id} was not found",
-        )
 
     if not nation:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Nation with id {nation_id} was not found",
         )
-
-    if manager.current == True:
-        new_manager.date_end = None
-
-    if manager.current == False:
-        if not manager.date_end:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"Managers which are not current must have an date end",
-            )
-        if manager.date_start >= manager.date_end:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"Manager start date must be before manager end date",
-            )
 
     db.add(new_manager)
     db.commit()
@@ -70,21 +47,13 @@ def create_manager(
 
 @router.get("/", response_model=Page[schemas.ManagerResponse])
 def get_managers(
-    current: bool | None = None,
     nation: int | None = None,
-    team: int | None = None,
     api_key: str = Security(get_api_key),
     db: Session = Depends(get_db),
 ):
     managers_query = select(Manager).order_by(Manager.id)
-    if current != None:
-        managers_query = managers_query.filter(Manager.current == current)
-
     if nation:
         managers_query = managers_query.filter(Manager.nationality == nation)
-
-    if team:
-        managers_query = managers_query.filter(Manager.team == team)
 
     return paginate(db, managers_query)
 
@@ -150,39 +119,15 @@ def update_manager(
         )
 
     updated_manager = updated_manager.model_dump()
-    team_id = updated_manager.get("team")
-    nation_id = updated_manager.get("nationality")
+    nation_id = updated_manager.get("nation_id")
 
-    team = db.query(Team).filter(Team.id == team_id).first()
     nation = db.query(Nation).filter(Nation.id == nation_id).first()
-
-    if not team:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Team with id {team_id} was not found",
-        )
 
     if not nation:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Nation with id {nation_id} was not found",
         )
-
-    if updated_manager.get("current") == True:
-        updated_manager["date_end"] = None
-
-    if updated_manager.get("current") == False:
-        if not updated_manager.get("date_end"):
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"Managers which are not current must have an date end",
-            )
-
-        if updated_manager.get("date_start") >= updated_manager.get("date_end"):
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail=f"Manager start date must be before manager end date",
-            )
 
     manager_query.update(updated_manager, synchronize_session=False)
     db.commit()
