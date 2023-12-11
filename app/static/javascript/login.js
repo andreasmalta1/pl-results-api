388a2880578e143a43d3701971f3bfba8e7cf084
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2023-12-11 16:08:24 +0100

    Added login JS

---
+++ app/static/javascript/login.js
@@ -0,0 +1,24 @@
+const inputBtn = document.querySelector(".generate-btn")
+const emailEl = document.querySelector("#email")
+const passwordEl = document.querySelector("#password")
+const messagesEl = document.querySelector("#messages")
+
+inputBtn.addEventListener("click", function (e) {
+  messagesEl.innerHTML = ""
+  if (!emailEl.value) {
+    e.preventDefault()
+    const messageEl = document.createElement("h4")
+    messageEl.textContent = "Please input an email address"
+    messageEl.classList.add("message")
+    messagesEl.appendChild(messageEl)
+    return false
+  }
+  else if (!passwordEl.value) {
+    e.preventDefault()
+    const messageEl = document.createElement("h4")
+    messageEl.textContent = "Please input a password"
+    messageEl.classList.add("message")
+    messagesEl.appendChild(messageEl)
+    return false
+  }
+})
